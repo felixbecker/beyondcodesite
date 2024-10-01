673a82fa4fc9ffd03b0ac5c36c51ae84c050c02c
Felix from Macbook <mail@felixbecker.net>
2024-10-01 07:48:20 +0200

    cleaned up project and started without theme. Tailwindcss is not working right now and needs a fix

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    './layouts/**/*.html',
+    './content/**/*.md',
+    './assets/**/*.js',
+  ],
+  darkMode: false, // oder 'media' oder 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
